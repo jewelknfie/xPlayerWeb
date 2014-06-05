009a6c7d9e093f243ad8e3e87b2bc30c21ce8aee
jewelknife <jewelknife@qq.com>
2014-06-05 23:47:58 +0800

    add

--- public/js/fuc.js
+++ public/js/fuc.js
@@ -3,7 +3,8 @@ function showPlayer(obj, title, videoName) {
     $("#vplayer").attr("src","http://localhost/video/" + videoName);//直接播放
 //    $("#vplayer").attr("type","video/" + /\.[^\.]+/.exec(videoName));//直接播放
     $("#vplayer").attr("autoplay","true");//直接播放
-    $(obj).addClass("active"); 
+	
+	$(obj).toggleClass("active"); 
     
 }
 
